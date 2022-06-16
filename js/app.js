be9906559c0391d83b65ffcf4e47b03c5102948a
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-06-16 16:20:00 -0500

    added about page and rating js function

--- js/app.js
+++ js/app.js
@@ -17,30 +17,44 @@ function askcolor() {
 }
 
 function guessingGame() {
-    let difficulty
-    let target
-    let attempts = 0
-    let guess
+    let difficulty;
+    let target;
+    let attempts = 0;
+    let guess;
     do  {
-        difficulty = parseInt(prompt('How many numbers do you want to guess from?'))
+        difficulty = parseInt(prompt('How many numbers do you want to guess from?'));
         if (isNaN(difficulty)) {
-            alert('That is not a number. Stop being difficult.')
+            alert('That is not a number. Stop being difficult.');
         }
-    } while(isNaN(difficulty))
+    } while(isNaN(difficulty));
 
-    target = Math.floor(Math.random() * (difficulty)) + 1
+    target = Math.floor(Math.random() * (difficulty)) + 1;
     do {
-        guess = parseInt(prompt('Guess a number between 1 and ' + difficulty + '.'))
-        attempts++
+        guess = parseInt(prompt('Guess a number between 1 and ' + difficulty + '.'));
+        attempts++;
         if (isNaN(guess)) {
-            alert('That is not a number. Stop being difficult.')
+            alert('That is not a number. Stop being difficult.');
         }
-    } while(target != guess)
+    } while(target != guess);
     if(attempts == 1) {
-        alert('Wow! You guessed it on your first try!')
+        alert('Wow! You guessed it on your first try!');
     } else if(attempts < 5) {
-        alert('That\'s right! It took you ' + attempts + ' guesses.')
+        alert('That\'s right! It took you ' + attempts + ' guesses.');
     } else {
-        alert('Ugh, finally. It took you ' + attempts + ' guesses.')
+        alert('Ugh, finally. It took you ' + attempts + ' guesses.');
     }
+}
+
+function rate() {
+    console.log('It is running')
+    let rating = parseInt(prompt('On a scale of 1 to 10, how clean are we?'));
+    let ratingOutput = '';
+    while (isNaN(rating)) {
+        rating = parseInt(prompt('That is not a number.\nOn a scale of 1 to 10, how clean are we?'));
+    }
+    for(let i = 0; i < rating; i++) {
+        ratingOutput += '<img src="images/CleaningBucket.png" />';
+        ratingOutput += i;
+    }
+    return document.write(ratingOutput);
 }
