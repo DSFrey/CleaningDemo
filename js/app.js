f7bbacd350b46fb025dea8a693682e451cf06ec0
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-06-16 14:09:17 -0500

    Added js guessing game

--- js/app.js
+++ js/app.js
@@ -15,3 +15,32 @@ function askcolor() {
     alert('I do not know that color');
     }
 }
+
+function guessingGame() {
+    let difficulty
+    let target
+    let attempts = 0
+    let guess
+    do  {
+        difficulty = parseInt(prompt('How many numbers do you want to guess from?'))
+        if (isNaN(difficulty)) {
+            alert('That is not a number. Stop being difficult.')
+        }
+    } while(isNaN(difficulty))
+
+    target = Math.floor(Math.random() * (difficulty)) + 1
+    do {
+        guess = parseInt(prompt('Guess a number between 1 and ' + difficulty + '.'))
+        attempts++
+        if (isNaN(guess)) {
+            alert('That is not a number. Stop being difficult.')
+        }
+    } while(isNaN(guess))
+    if(attempts == 1) {
+        alert('Wow! You guessed it on your first try!')
+    } else if(attempts < 5) {
+        alert('That\'s right! It took you ' + attempts + ' guesses.')
+    } else {
+        alert('Ugh, finally. It took you ' + attempts + ' guesses.')
+    }
+}
