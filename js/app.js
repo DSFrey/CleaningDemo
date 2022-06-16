063cf633de71db538a28e6f7c267e73d2e93d217
Daniel Frey <daniel.stephen.frey@gmail.com>
2022-06-15 22:45:27 -0500

    Cleaned up css and js

--- js/app.js
+++ js/app.js
@@ -1,42 +1,14 @@
 'use strict';
 console.log('app js file is connected');
 
-// let userName = prompt('What is your name?');
-
-// console.log('Well hello there,',userName);
-
-// let age = prompt('How old are you');
-
-// let ageNum = parseInt(age);
-
-// console.log('You are',ageNum);
-
-// let message;
-
-// if(age <= 12){
-//     message = 'Aw, you\'re cute';
-// } else if(age <= 19){
-//     message = 'GROW UP';
-// } else if(age > 19){
-//     message = 'You are old';
-// } else {
-//     message = 'I need a number'
-// }
-
-// console.log(message);
-
-// document.getElementById(askcolor)
-
 function iscolor(strcolor) {
     let s = new Option().style;
     s.color = strcolor;
     return s.color == strcolor;
 }
 
-
-
 function askcolor() {
-    let color = prompt('What color do you want?');
+    let color = prompt('What color do you want?').toLowerCase();
     if(iscolor(color)){
         document.getElementById('background').style.backgroundColor = color;
     } else {
